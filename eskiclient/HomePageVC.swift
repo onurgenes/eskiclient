63f0fc7498094823d8754e2c46329b29c6f3de9b
Onur Genes <onur@onurgenes.com>
2019-09-07 15:24:41 +0300

    trying to get data

--- eskiclient/HomePageVC.swift
+++ eskiclient/HomePageVC.swift
@@ -22,43 +22,39 @@ class HomePageVC: UIViewController {
                 print("FAIL: \(err)")
             case .success(let val):
                 if let doc = try? HTML(html: val, encoding: .utf8) {
-                    print(doc.title)
-                    for baslik in doc.xpath("//div[@id='index-section']//li/a") {
-                        print(baslik.text!)
-                        print(baslik["href"]!)
+                    for baslik in doc.xpath("//li/a") {
+                        print(baslik.text)
+                        
+                        // just "başlık name"
+                        print(baslik.xpath("text()[1]").first?.text?.trimmingCharacters(in: .whitespaces))
+                        
+                        // just "başlık new entry count"
+                        print(baslik.xpath("small/text()[1]").first?.text?.trimmingCharacters(in: .whitespaces))
+                        print(baslik["href"])
                     }
                 }
             }
         }
+        // FOR GETTING "entries" of "başlık"
         
-        AF.request("https://eksisozluk.com/basliklar/bugun/1").responseString { (response) in
-            switch response.result {
-            case .failure(let err):
-                print("FAIL: \(err)")
-            case .success(let val):
-                if let doc = try? HTML(html: val, encoding: .utf8) {
-                    print(doc.title!)
-                    for baslik in doc.xpath("//div[@id='index-section']//li/a") {
-                        print(baslik.text!)
-                        print(baslik["href"]!)
-                    }
-                    AF.request("https://eksisozluk.com\(doc.xpath("//div[@id='index-section']//li/a").first!["href"]!)").responseString(completionHandler: { (res) in
-                        //ul[@id='entry-item-list']/li
-                        switch res.result {
-                        case .failure(let err):
-                            print("FAIL: \(err)")
-                        case .success(let aa):
-                            if let aaa = try? HTML(html: aa, encoding: .utf8) {
-//                                for something in aaa.xpath("//ul[@id='entry-item-list']/li/div") {
-//                                    print(something.text!.trimmingCharacters(in: .whitespacesAndNewlines))
+//                    AF.request("https://eksisozluk.com\(doc.xpath("//li/a").first!["href"]!)").responseString(completionHandler: { (res) in
+//                        //ul[@id='entry-item-list']/li
+//                        switch res.result {
+//                        case .failure(let err):
+//                            print("FAIL: \(err)")
+//                        case .success(let aa):
+//                            if let aaa = try? HTML(html: aa, encoding: .utf8) {
+//                                //                                for something in aaa.xpath("//ul[@id='entry-item-list']/li/div") {
+//                                //                                    print(something.text!.trimmingCharacters(in: .whitespacesAndNewlines))
+//                                //                                }
+//                                let html = aaa.xpath("//ul[@id='entry-item-list']/li/div").first!.toHTML!
+//                                let data = Data(html.utf8)
+//                                if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
+//                                    print(attributedString)
 //                                }
-                                print(aaa.xpath("//ul[@id='entry-item-list']/li/div").first!.toHTML!)
-                            }
-                        }
-                    })
-                }
-            }
-        }
+//                            }
+//                        }
+//                    })
     }
 }
 
